c4e20ba78022c58ff0e16d96436023a8322265c5
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-04-09 16:43:49 -0300

    Abordagem de otimização

---
+++ e2e/specs/edicaoDePerfil.spec.ts
@@ -0,0 +1,21 @@
+import test from "@playwright/test";
+
+test.describe("Página de perfil", () => {
+  test("Editar perfil 1", () => {
+    // fazer cadastro de um novo perfil
+
+    // fazer login desse perfil
+
+    // editar esse perfil
+  });
+
+  test("Editar perfil 2", () => {
+    // fazer cadastro de um novo perfil
+
+    // fazer login desse perfil
+
+    // editar esse perfil
+  });
+
+  // 50 testes de edição de perfil -> 150 testes nos 3 navegadores
+});
