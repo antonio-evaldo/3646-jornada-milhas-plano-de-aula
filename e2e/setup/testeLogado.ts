8d993f85f948fd22e910b5706be92a84303ed625
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-04-10 17:21:02 -0300

    Criando arquivos de login

--- e2e/setup/testeLogado.ts
+++ e2e/setup/testeLogado.ts
@@ -1,15 +1,38 @@
 import * as path from "path";
+import * as fs from "fs";
+import { expect } from "@playwright/test";
+
 import { test } from "./fixtures";
+import PaginaCadastro from "e2e/page-objects/PaginaCadastro";
+import PaginaLogin from "e2e/page-objects/PaginaLogin";
+import { gerarPerfil } from "e2e/operacoes/gerarPerfil";
 
 export const testeLogado = test.extend<object, { arquivoInfoLogin: string }>({
   arquivoInfoLogin: [async ({ browser }, use) => {
     const id = test.info().parallelIndex;
-    const caminhoArquivo = path.resolve(test.info().project.outputDir, `.auth/${id}.json`);
+    const caminhoArquivo = path.resolve(test.info().project.outputDir, `.auth/usuario-${id}.json`);
 
-    console.log('Índice paralelo do worker:', id);
-    console.log('Caminho do arquivo de login desse worker:', caminhoArquivo);
+    if (fs.existsSync(caminhoArquivo)) {
+      await use(caminhoArquivo);
+      return;
+    }
+
+    const page = await browser.newPage({ storageState: undefined });
+    const paginaCadastro = new PaginaCadastro(page);
+    const paginaLogin = new PaginaLogin(page);
+    const novoUsuario = gerarPerfil();
+
+    await page.goto('http://localhost:4200/auth/cadastro');
+    await paginaCadastro.cadastrarUsuario(novoUsuario);
+    await expect(page).toHaveURL('http://localhost:4200/auth/login');
+
+    await paginaLogin.fazerLogin(novoUsuario.email, novoUsuario.senha);
+    await expect(page).toHaveURL('http://localhost:4200/home');
+
+    await page.context().storageState({ path: caminhoArquivo });
 
     await use(caminhoArquivo);
+    await page.close();
   }, { scope: 'worker' }],
   storageState: ({ arquivoInfoLogin }, use) => use(arquivoInfoLogin)
 });
